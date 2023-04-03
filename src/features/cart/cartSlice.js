61432c2ec09e4987fe726dce5fa0709856a0f74b
Nahom <zerihunnahom@gmail.com>
2023-04-04 01:07:59 +0300

    Add remove item action

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,8 +12,13 @@
     reducers:{
       clearCart:(state)=>{
          state.cartItems=[]
+      },
+      removeItem:(state,action)=>{
+         console.log(action)
+         const itemId = action.payload
+         state.cartItems = state.cartItems.filter(item=>item.id !== itemId)
       }
     }
  })
-export const {clearCart} = cartSlice.actions
+export const {clearCart, removeItem} = cartSlice.actions
  export default cartSlice.reducer
